b90bacac4d6802934f85cd6275b32f3857e2ebfd
Gaurav <gpdhande12@gmail.com>
2024-07-09 23:03:33 +0530

    checks mood

--- src/main/java/org/MoodAnalyser/MoodAnalyzer.java
+++ src/main/java/org/MoodAnalyser/MoodAnalyzer.java
@@ -2,11 +2,22 @@ package org.MoodAnalyser;
 
 public class MoodAnalyzer 
 {
-	
+	public String analyseMood(String message) 
+	{
+        message = message.toLowerCase();
+        if(message.contains("sad")){
+            return "SAD";
+        }else if(message.contains("happy")){
+            return "HAPPY";
+        }else{
+            return "";
+        }
+    }
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcome to Mood Analyser Test");
+		//System.out.println("Welcome to Mood Analyser Test");
 	}
+
 
 }
